d0e71eb455d3e398d93e8ab3d2309afe9a00e2e1
Robert McLay <mclay@zaniahgroup.com>
2014-01-06 15:48:31 -0600

    term size

--- lib/getTermalSize.py
+++ lib/getTermalSize.py
@@ -12,37 +12,42 @@ def getTerminalSize():
       import fcntl, termios
       return struct.unpack("hh", fcntl.ioctl(fd, termios.TIOCGWINSZ, "1234"))
   # try stdin, stdout, stderr
-  for fd in (0, 1, 2):
-      try:
-          return ioctl_GWINSZ(fd)
-      except:
-          pass
+  if (not sys.stdout.isatty()):
+    return (25, 80)
+
+  fd = 1
+  try:
+    return ioctl_GWINSZ(fd)
+  except:
+    pass
   # try os.ctermid()
   try:
-      fd = os.open(os.ctermid(), os.O_RDONLY)
-      try:
-          return ioctl_GWINSZ(fd)
-      finally:
-          os.close(fd)
+    fd = os.open(os.ctermid(), os.O_RDONLY)
+    try:
+      return ioctl_GWINSZ(fd)
+    finally:
+      os.close(fd)
   except:
-      pass
+    pass
   # try `stty size`
   try:
-      return tuple(int(x) for x in os.popen("stty size", "r").read().split())
+    return tuple(int(x) for x in os.popen("stty size", "r").read().split())
   except:
-      pass
+    pass
   # try environment variables
   try:
-      return tuple(int(os.getenv(var)) for var in ("LINES", "COLUMNS"))
+    return tuple(int(os.getenv(var)) for var in ("LINES", "COLUMNS"))
   except:
-      pass
+    pass
   # i give up. return default.
   return (25, 80)
 
 def main():
   rows, columns = getTerminalSize()
   
-  print(rows, columns)
+  s = "rows: "+str(rows)+" columns: "+str(columns)+"\n"
+
+  sys.stderr.write(s)
 
 
 if ( __name__ == '__main__'): main()
--- rtm/ReportResults.py
+++ rtm/ReportResults.py
@@ -1,8 +1,9 @@
-from __future__ import print_function
-from BaseTask   import BaseTask
-from Engine     import MasterTbl, Error
-from Dbg        import Dbg
-from Tst        import Tst
+from __future__      import print_function
+from BaseTask        import BaseTask
+from Engine          import MasterTbl, Error
+from Dbg             import Dbg
+from Tst             import Tst
+from getTerminalSize import getTerminalSize
 import os
 
 dbg = Dbg()
@@ -12,4 +13,10 @@ class ReportResults(BaseTask):
     BaseTask.__init__(self, name)
 
   def execute(self, *args, **kwargs):
-    masterTbl = MasterTbl()
+    masterTbl   = MasterTbl()
+    rows, width = getTerminalSize()
+    rptT        = masterTbl['rptT']
+    humanDataA  = ()
+    tstSummaryT = masterTbl['tstSummaryT']
+    
+    
